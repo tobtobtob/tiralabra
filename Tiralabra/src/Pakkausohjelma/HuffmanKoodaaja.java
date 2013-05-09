58a2a9f6788a69b7f96ff6c72111f96a9d21e523
Topi <topi@topi-desktop.(none)>
2013-05-09 10:22:23 +0300

    Luokkarakennetta kehitetty, sanakirjan kirjoittaminen ja lukeminen lisätty

---
+++ Tiralabra/src/Pakkausohjelma/HuffmanKoodaaja.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pakkausohjelma;
+
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+/**
+ *
+ * @author topi
+ */
+public class HuffmanKoodaaja {
+
+    /**
+     * Metodi rakentaa annetusta kirjainten esiintymismäärät
+     * sisältävästä hajautustaulusta puun,
+     * josta voi selvittää kunkin merkin Huffman-koodin
+     *
+     * @param Merkkien esiintymistiheydet
+     * @return sanakirja puumuodossa
+     */
+    public Node rakennaPuu(HashMap<Character, Integer> aakkosto) {
+        PriorityQueue<Node> keko = new PriorityQueue<Node>();
+        for (char s : aakkosto.keySet()) {
+            Node n = new Node(s, aakkosto.get(s));
+            keko.add(n);
+        }
+        while (keko.size() > 1) {
+            Node node1 = keko.poll();
+            Node node2 = keko.poll();
+            Node uusi = new Node('_', node1.yleisyys + node2.yleisyys);
+            uusi.vasen = node1;
+            uusi.oikea = node2;
+            keko.add(uusi);
+        }
+        return keko.poll();
+    }
+    
+}
