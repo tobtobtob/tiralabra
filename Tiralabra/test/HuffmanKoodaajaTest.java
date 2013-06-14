0cefc033f5fda99d322bcf7421f2c0c7e0f2c9a3
Topi <topi@topi-desktop.(none)>
2013-06-14 19:35:34 +0300

    viimeiset commitit, luulisin

--- Tiralabra/test/HuffmanKoodaajaTest.java
+++ Tiralabra/test/HuffmanKoodaajaTest.java
@@ -61,9 +61,5 @@ public class HuffmanKoodaajaTest {
         
         
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+   
 }
